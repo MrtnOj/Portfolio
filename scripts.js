b55e011c08cb02a8525d6c331d90371850c82996
Martin Ojasalu <ojasalum@gmail.com>
2021-01-27 02:43:09 +0200

    mobile nav menu tweaked, cv page added

--- scripts.js
+++ scripts.js
@@ -1,21 +1,29 @@
- const hamburger = document.querySelector('#nav-hamburger');
- const mobileNavBar = document.querySelector('#mobile-nav');
- const mobileNavLink = document.querySelectorAll('.mobile-nav__link');
+const hamburger = document.querySelector('#nav-hamburger');
+const mobileNavBar = document.querySelector('#mobile-nav');
+const mobileNavLink = document.querySelectorAll('.mobile-nav__link');
+const backdrop = document.querySelector('.backdrop');
 
- const hamburgerClicked = (event) => {
-    if (mobileNavBar.style.display === 'none') {
-        mobileNavBar.style.display = 'block'
-    } else {
-        mobileNavBar.style.display = 'none'
-    }
- }
+// Function to open/close mobile sidebar and display backdrop
 
- const hideMobileNav = event => {
-    mobileNavBar.style.display = 'none'
- }
+const burgerOrBackdropClicked = event => {
+   if (mobileNavBar.style.display === 'none') {
+      mobileNavBar.style.display = 'block';
+      backdrop.style.display = 'block'
+   } else {
+      mobileNavBar.style.display = 'none';
+      backdrop.style.display = 'none'
+   }
+}
 
- hamburger.addEventListener('click', hamburgerClicked);
- 
- for (let i = 0 ; i < mobileNavLink.length; i++) {
-    mobileNavLink[i].addEventListener('click', hideMobileNav) ; 
- }
+hamburger.addEventListener('click', burgerOrBackdropClicked);
+backdrop.addEventListener('click', burgerOrBackdropClicked);
+
+
+// Listening on click events on menu items to close the mobile sidebar menu
+
+for (let i = 0 ; i < mobileNavLink.length; i++) {
+   mobileNavLink[i].addEventListener('click', event => {
+      mobileNavBar.style.display = 'none';
+      backdrop.style.display = 'none'
+   })
+}
